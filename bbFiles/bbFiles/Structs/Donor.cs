7c85661effc884d6a3e8a29e4309bfb6ca42a7c5
Mateusz Jamrowski <kermaxpl@gmail.com>
2017-05-24 09:45:33 +0200

    Further integration of donors system. Donor class constructors created

--- bbFiles/bbFiles/Structs/Donor.cs
+++ bbFiles/bbFiles/Structs/Donor.cs
@@ -10,5 +10,15 @@ namespace bbFiles.Structs
         public BloodTypes bloodtype { get; set; }
         public bool rhMarker { get; set; }
         public int PESEL { set; get; }
+        public Donor() { }
+        public Donor(string firstname, string surname, DateTime birthdate, BloodTypes bloodtype, bool rhMarker, int pesel)
+        {
+            this.firstname = firstname;
+            this.surname = surname;
+            this.birthdate = birthdate;
+            this.bloodtype = bloodtype;
+            this.rhMarker = rhMarker;
+            this.PESEL = pesel;
+        }
     }
 }
