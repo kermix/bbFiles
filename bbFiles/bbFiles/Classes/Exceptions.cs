f5ace10e7e1bc86c4a1f96fd63e71a4adeb496a3
Mateusz Jamrowski <mateuszjamrowski@outlook.com>
2017-07-18 13:02:08 +0200

    Added [Serializable] atributte to exceptions

--- bbFiles/bbFiles/Classes/Exceptions.cs
+++ bbFiles/bbFiles/Classes/Exceptions.cs
@@ -2,18 +2,22 @@
 
 namespace bbFiles
 {
+    [Serializable]
     public class UserNotLoggedException : Exception
     {
         public UserNotLoggedException(string msg) : base(msg) { }
     }
+    [Serializable]
     public class UndefinedUserRole : Exception
     {
         public UndefinedUserRole(string msg) : base(msg) { }
     }
+    [Serializable]
     public class InsufficientPrivilegesException : Exception
     {
         public InsufficientPrivilegesException(string msg) : base(msg) { }
     }
+    [Serializable]
     public class UserEditException : Exception
     {
         public UserEditException(string msg) : base(msg) { }
